85b7450f0b436e7544b9012678886c2a91622c5e
AsudEsma <asesozdemir@gmail.com>
2022-02-02 19:56:05 +0300

    Daredevil

--- src/test/java/FirstClassAsu.java
+++ src/test/java/FirstClassAsu.java
@@ -1,6 +1,11 @@
+import java.sql.SQLOutput;
+
 public class FirstClassAsu {
     public static void main(String[] args) {
         System.out.println("hi!");
+        System.out.println("You are dismissed Emre and Kadircan. Come at me if you will.");
+
+
     }
 
 }
