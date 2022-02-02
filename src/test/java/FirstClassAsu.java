be756b6bc9923c8e6b8ba9cd98bff2219bc87b13
AsudEsma <asesozdemir@gmail.com>
2022-02-02 20:00:45 +0300

    3rd commit

--- src/test/java/FirstClassAsu.java
+++ src/test/java/FirstClassAsu.java
@@ -4,6 +4,7 @@ public class FirstClassAsu {
     public static void main(String[] args) {
         System.out.println("hi!");
         System.out.println("You are dismissed Emre and Kadircan. Come at me if you will.");
+        System.out.println("Selamlar Kadircan hocam");
 
 
     }
